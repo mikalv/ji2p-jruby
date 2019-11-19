b61031b72f735cf06d0006eacbef893ed621e071
Mikal Villa <mikalv@mikalv.net>
2019-11-19 05:56:32 +0100

    Don't depend on active_support for now

--- lib/ji2p.rb
+++ lib/ji2p.rb
@@ -1,5 +1,11 @@
-require 'active_support/dependencies'
+#require 'active_support'
+#require 'active_support/core_ext'
+#require 'active_support/dependencies'
 require 'logger'
+require 'java'
+$CLASSPATH << "file:///#{File.expand_path(File.join(__dir__, '..', 'config'))}/"
+
+require 'ji2p/environment.rb'
 
 module Ji2p
   require_relative 'ji2p/version.rb'
@@ -7,13 +13,25 @@ module Ji2p
   autoload :Control, File.expand_path('ji2p/control.rb', __dir__)
   autoload :Server,  File.expand_path('ji2p/server.rb', __dir__)
   autoload :Startup, File.expand_path('ji2p/startup.rb', __dir__)
-  ActiveSupport::Dependencies.autoload_paths << __dir__
-  Dir.glob('**/').each do |dir|
-    ActiveSupport::Dependencies.autoload_paths << File.expand_path(dir, __dir__)
-  end
+  #ActiveSupport::Dependencies.autoload_paths << __dir__
+  #Dir.glob('**/').each do |dir|
+  #  ActiveSupport::Dependencies.autoload_paths << File.expand_path(dir, __dir__)
+  #end
+
+  # https://github.com/jruby/jruby/wiki/RedBridge
+  java_import 'java.lang.System'
+  System.setProperty("org.jruby.embed.localcontext.scope", "singleton") # concurrent, threadsafe, etc.
 
   def self.logger
     @logger ||= Logger.new(STDOUT)
+  end
+
+  def self.disable_crypto_restriction
+    # java.lang.Class.for_name('javax.crypto.JceSecurity').get_declared_field('isRestricted').tap{|f| f.accessible = true; f.set nil, false}
+    security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
+    restricted_field = security_class.get_declared_field('isRestricted')
+    restricted_field.accessible = true
+    restricted_field.set nil, false
   end
 
 end
